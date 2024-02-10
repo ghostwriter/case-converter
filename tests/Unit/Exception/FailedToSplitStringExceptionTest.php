5116f07f12300abd6722264793800e59a8876b2b
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-02-10 13:37:13 -0600

    Create FailedToSplitStringExceptionTest.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

---
+++ tests/Unit/Exception/FailedToSplitStringExceptionTest.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ghostwriter\CaseConverterTests\Unit\Exception;
+
+use Ghostwriter\CaseConverter\Exception\FailedToSplitStringException;
+use Ghostwriter\CaseConverter\Interface\Exception\CaseConverterExceptionInterface;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
+
+#[CoversClass(FailedToSplitStringException::class)]
+final class FailedToSplitStringExceptionTest extends TestCase
+{
+    final public function testInstanceOfCaseConverterExceptionInterface(): void
+    {
+        self::assertInstanceOf(CaseConverterExceptionInterface::class, new FailedToSplitStringException());
+    }
+}
