fec4c922177626187d2e9ba20313d028fff766d5
Nathanael Esayeas <nathanael.esayeas@protonmail.com>
2024-06-11 11:15:03 -0500

    Update CaseConverter.php
    Signed-off-by: Nathanael Esayeas <nathanael.esayeas@protonmail.com>

--- src/CaseConverter.php
+++ src/CaseConverter.php
@@ -7,6 +7,13 @@ namespace Ghostwriter\CaseConverter;
 use Closure;
 use Ghostwriter\CaseConverter\Exception\FailedToSplitStringException;
 use Ghostwriter\CaseConverter\Interface\CaseConverterInterface;
+use Override;
+
+use const MB_CASE_LOWER;
+use const MB_CASE_TITLE;
+use const MB_CASE_UPPER;
+use const PREG_SPLIT_DELIM_CAPTURE;
+use const PREG_SPLIT_NO_EMPTY;
 
 use function array_map;
 use function implode;
@@ -17,12 +24,6 @@ use function preg_split;
 use function str_contains;
 use function ucfirst;
 
-use const MB_CASE_LOWER;
-use const MB_CASE_TITLE;
-use const MB_CASE_UPPER;
-use const PREG_SPLIT_DELIM_CAPTURE;
-use const PREG_SPLIT_NO_EMPTY;
-
 /**
  * @psalm-immutable
  *
@@ -30,10 +31,19 @@ use const PREG_SPLIT_NO_EMPTY;
  */
 final readonly class CaseConverter implements CaseConverterInterface
 {
+    /**
+     * @var pure-Closure(string):string
+     */
     private Closure $lower;
 
+    /**
+     * @var pure-Closure(string):string
+     */
     private Closure $title;
 
+    /**
+     * @var pure-Closure(string):string
+     */
     private Closure $upper;
 
     public function __construct()
@@ -43,91 +53,159 @@ final readonly class CaseConverter implements CaseConverterInterface
         $this->title = static fn (string $word): string => mb_convert_case($word, MB_CASE_TITLE, 'UTF-8');
     }
 
+    /**
+     * @throws FailedToSplitStringException
+     */
+    #[Override]
     public function adaCase(string $string): string
     {
         return $this->glueUnderscore($this->split($string), $this->title);
     }
 
+    /**
+     * @throws FailedToSplitStringException
+     */
+    #[Override]
     public function camelCase(string $string): string
     {
         return lcfirst($this->glueUppercase($this->split($string), $this->title));
     }
 
+    /**
+     * @throws FailedToSplitStringException
+     */
+    #[Override]
     public function cobolCase(string $string): string
     {
         return $this->glueDash($this->split($string), $this->upper);
     }
 
+    /**
+     * @throws FailedToSplitStringException
+     */
+    #[Override]
     public function dotCase(string $string): string
     {
         return $this->glueDot($this->split($string), $this->lower);
     }
 
+    /**
+     * @throws FailedToSplitStringException
+     */
+    #[Override]
     public function kebabCase(string $string): string
     {
         return $this->glueDash($this->split($string), $this->lower);
     }
 
+    /**
+     * @throws FailedToSplitStringException
+     */
+    #[Override]
     public function lowerCase(string $string): string
     {
         return $this->glueSpace($this->split($string), $this->lower);
     }
 
+    /**
+     * @throws FailedToSplitStringException
+     */
+    #[Override]
     public function macroCase(string $string): string
     {
         return $this->glueUnderscore($this->split($string), $this->upper);
     }
 
+    /**
+     * @throws FailedToSplitStringException
+     */
+    #[Override]
     public function pascalCase(string $string): string
     {
         return $this->glueUppercase($this->split($string), $this->title);
     }
 
+    /**
+     * @throws FailedToSplitStringException
+     */
+    #[Override]
     public function sentenceCase(string $string): string
     {
         return ucfirst($this->glueSpace($this->split($string), $this->lower));
     }
 
+    /**
+     * @throws FailedToSplitStringException
+     */
+    #[Override]
     public function snakeCase(string $string): string
     {
         return $this->glueUnderscore($this->split($string), $this->lower);
     }
 
+    /**
+     * @throws FailedToSplitStringException
+     */
+    #[Override]
     public function titleCase(string $string): string
     {
         return $this->glueSpace($this->split($string), $this->title);
     }
 
+    /**
+     * @throws FailedToSplitStringException
+     */
+    #[Override]
     public function trainCase(string $string): string
     {
         return $this->glueDash($this->split($string), $this->title);
     }
 
+    /**
+     * @throws FailedToSplitStringException
+     */
+    #[Override]
     public function upperCase(string $string): string
     {
         return $this->glueSpace($this->split($string), $this->upper);
     }
 
+    /**
+     * @param pure-Closure(string):string $converter
+     */
     private function glueDash(array $words, Closure $converter): string
     {
         return implode('-', $this->map($converter, $words));
     }
 
+
+    /**
+     * @param pure-Closure(string):string $converter
+     */
     private function glueDot(array $words, Closure $converter): string
     {
         return implode('.', $this->map($converter, $words));
     }
 
+    /**
+     * @param pure-Closure(string):string $converter
+     */
     private function glueSpace(array $words, Closure $converter): string
     {
         return implode(' ', $this->map($converter, $words));
     }
 
+    /**
+     * @param pure-Closure(string):string $converter
+     */
     private function glueUnderscore(array $words, Closure $converter): string
     {
         return implode('_', $this->map($converter, $words));
     }
 
+    /**
+     * @param pure-Closure(string):string $converter
+     */
     private function glueUppercase(array $words, Closure $converter): string
     {
         return implode('', $this->map($converter, $words));
@@ -142,21 +220,26 @@ final readonly class CaseConverter implements CaseConverterInterface
     }
 
     /**
-     * @return list<string>
+     * @param pure-Closure(string):string $converter
      *
-     * @psalm-pure
+     * @return list<string>
      */
     private function map(Closure $converter, array $words): array
     {
         /**
-         * @var list<string>           $words
-         * @var Closure(string):string $converter
+         * @var pure-Closure(string):string $converter
+         * @var list<string>                $words
          *
          * @return list<string>
          */
         return array_map($converter, $words);
     }
 
+    /**
+     * @throws FailedToSplitStringException
+     *
+     * @return list<string>
+     */
     private function split(string $string): array
     {
         if (str_contains($string, '_')) {
@@ -182,33 +265,60 @@ final readonly class CaseConverter implements CaseConverterInterface
         return $this->splitUppercase($string);
     }
 
+    /**
+     * @throws FailedToSplitStringException
+     *
+     * @return list<string>
+     */
     private function splitDash(string $string): array
     {
         return $this->words($string, '#-+#u');
     }
 
+    /**
+     * @throws FailedToSplitStringException
+     *
+     * @return list<string>
+     */
     private function splitDot(string $string): array
     {
         return $this->words($string, '#\\.+#u');
     }
 
+    /**
+     * @throws FailedToSplitStringException
+     *
+     * @return list<string>
+     */
     private function splitSpace(string $string): array
     {
         return $this->words($string, '# +#u');
     }
 
+    /**
+     * @throws FailedToSplitStringException
+     *
+     * @return list<string>
+     */
     private function splitUnderscore(string $string): array
     {
         return $this->words($string, '#_+#u');
     }
 
+    /**
+     * @throws FailedToSplitStringException
+     *
+     * @return list<string>
+     */
     private function splitUppercase(string $string): array
     {
         return $this->words($string, '#(?=\p{Lu}{1})#u');
     }
 
     /**
-     * @psalm-pure
+     * @throws FailedToSplitStringException
+     *
+     * @return list<string>
      */
     private function words(string $string, string $pattern): array
     {
